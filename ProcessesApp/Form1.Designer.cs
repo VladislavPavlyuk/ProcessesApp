f7048be0f0231a327ea8bc1bc1edd4f629a622d6
vladp <vladp@DESKTOP-A3O7OP4>
2023-11-01 22:10:58 +0100

    Add project files.

---
+++ ProcessesApp/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace ProcessesApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            activeProcessList = new ListBox();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(52, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(699, 29);
+            button1.TabIndex = 0;
+            button1.Text = "Refresh list of processes";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(52, 47);
+            button2.Name = "button2";
+            button2.Size = new Size(699, 29);
+            button2.TabIndex = 1;
+            button2.Text = "End process";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(52, 409);
+            button3.Name = "button3";
+            button3.Size = new Size(699, 29);
+            button3.TabIndex = 2;
+            button3.Text = "New process";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // activeProcessList
+            // 
+            activeProcessList.FormattingEnabled = true;
+            activeProcessList.ItemHeight = 20;
+            activeProcessList.Location = new Point(56, 111);
+            activeProcessList.Name = "activeProcessList";
+            activeProcessList.Size = new Size(695, 264);
+            activeProcessList.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(342, 88);
+            label1.Name = "label1";
+            label1.Size = new Size(118, 20);
+            label1.TabIndex = 4;
+            label1.Text = "Active processes";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(55, 381);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(696, 27);
+            textBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(activeProcessList);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Task Manager";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private ListBox activeProcessList;
+        private Label label1;
+        private TextBox textBox1;
+    }
+}
---
+++ ProcessesApp/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+using System.Security.Principal;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace ProcessesApp
+{
+    public partial class Form1 : Form
+    {
+        public SynchronizationContext uiContext;
+        public Form1()
+        {
+            InitializeComponent();
+            // Получим контекст синхронизации для текущего потока 
+            uiContext = SynchronizationContext.Current;
+            uiContext.Send(d => activeProcessList.Items.Clear(), null);
+            Process[] lp = Process.GetProcesses();
+            foreach (Process p in lp) // список всех процессов, запущенных в системе
+            {
+                // uiContext.Send отправляет синхронное сообщение в контекст синхронизации
+                // SendOrPostCallback - делегат указывает метод, вызываемый при отправке сообщения в контекст синхронизации. 
+                uiContext.Send(d => activeProcessList.Items.Add(p.ProcessName) /* Вызываемый делегат SendOrPostCallback */,
+                    null /* Объект, переданный делегату */);// получим имя очередного процесса
+            }
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            await Task.Run(() =>
+            {
+                try
+                {
+                    uiContext.Send(d => activeProcessList.Items.Clear(), null);
+                    Process[] lp = Process.GetProcesses();
+                    foreach (Process p in lp) // список всех процессов, запущенных в системе
+                    {
+                        // uiContext.Send отправляет синхронное сообщение в контекст синхронизации
+                        // SendOrPostCallback - делегат указывает метод, вызываемый при отправке сообщения в контекст синхронизации. 
+                        uiContext.Send(d => activeProcessList.Items.Add(p.ProcessName) /* Вызываемый делегат SendOrPostCallback */,
+                            null /* Объект, переданный делегату */);// получим имя очередного процесса
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            });
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (activeProcessList.SelectedIndex == -1)  //Если процесс не выбран
+            {
+                MessageBox.Show("You didn't chosen any process to delete.");
+
+                return;
+            }
+
+            // To kill all processes by name
+            Process[] ps = Process.GetProcessesByName(activeProcessList.Text);
+            foreach (Process p in ps) p.Kill();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // создаем новый процесс
+                Process proc = new Process();
+                // Запускаем процесс
+                proc.StartInfo.FileName = textBox1.Text;
+                //proc.StartInfo.Arguments = Application.ExecutablePath;
+                proc.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+
+}
